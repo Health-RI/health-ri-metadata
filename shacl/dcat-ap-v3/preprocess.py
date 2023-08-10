e0b2f468e5a794fa6ec30851d5635d451f191bb7
anastasianayden <anastasia.nayden@health-ri.nl>
2023-08-10 15:33:22 +0200

    replace dict with defaultdict

--- shacl/dcat-ap-v3/preprocess.py
+++ shacl/dcat-ap-v3/preprocess.py
@@ -1,14 +1,19 @@
 from rdflib import Graph, URIRef, Namespace, RDF
+from rdflib.term import Node
+from typing import Union
 from hashlib import sha1
+from collections import defaultdict
 
 SHACL = Namespace("http://www.w3.org/ns/shacl#")
 PURL_EU_SHACL = Namespace("https://purl.eu/ns/shacl#")
 
-def hash(nodeshape: URIRef, path: URIRef) -> URIRef:
+
+def hash(nodeshape: Union[URIRef, Node], path: Union[URIRef, Node]) -> URIRef:
     base = str(nodeshape)
 
     hashed = sha1((nodeshape+path).encode("UTF-8"))
     return URIRef(f"{base}/{hashed.hexdigest()}")
+
 
 def preprocess(source: str) -> None:
     g = Graph().parse(location=source, format="n3")
@@ -17,16 +22,13 @@ def preprocess(source: str) -> None:
     for nodeshape in g.subjects(RDF.type, SHACL.NodeShape):
         print(f"Found nodeshape {nodeshape}")
 
-        merged = {}
+        merged = defaultdict(Graph)
 
         for propertyshape in g.objects(nodeshape, SHACL.property):
             # print(f" Found propertyshape {propertyshape}")
 
             path = g.value(subject=propertyshape, predicate=SHACL.path)
             # print(f"  concerning path {path}")
-
-            if path not in merged:
-                merged[path] = Graph()
 
             subj = hash(nodeshape, path) #URIRef(f"foo:{sha1((nodeshape+path).encode('utf-8')).hexdigest()}")
             mg = merged[path]
@@ -45,8 +47,8 @@ def preprocess(source: str) -> None:
 
             output.add((s, p, o))
 
-        for path in merged:
-            graph = merged[path]
+        for value in merged.values():
+            graph = value
 
             for s in graph.subjects():
                 output.add((nodeshape, SHACL.property, s))
@@ -55,6 +57,7 @@ def preprocess(source: str) -> None:
             output += graph
 
     print(f"total merged looks like:\n{output.serialize()}")
+
 
 if __name__ == "__main__":
     release = "https://github.com/SEMICeu/DCAT-AP/raw/master/releases/3.0.0/shacl/dcat-ap-SHACL.ttl"
