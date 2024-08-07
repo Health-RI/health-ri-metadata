6014f13615fcfa1c0f4c430ac130a1e8a5c60348
Xiaofeng Liao <xiaofengleo@users.noreply.github.com>
2024-08-07 11:45:33 +0200

    feat: revise the mincount and maxcount (#132)
    * fix: revise the mincount and maxcount
    
    revise the mincount and maxcount for, align with the latest core metadata diagram on develp branch page.
    Also remove legacy relation pointing to foaf:project
    change contact point cardinality back to maxcount 1.
    remove maxcount 1 for publisher
    contact point and publisher are mandatory, i.e., mincount 1.
    Also revise the example dataset, where contactpoint and publisher are both present in the examples.
    accordingly, add modified and license in example dataset
    
    Co-authored-by: Mark Janse <mark.janse@health-ri.nl>

--- tests/test_shacl.py
+++ tests/test_shacl.py
@@ -23,15 +23,20 @@ def test_core_shapes(shacl, example):
     )
     assert conform, result_text
 
+
 @pytest.mark.parametrize(
     ("shacl", "example"),
     [
         ("Dataset", "example-dataset-bad"),
+        ("Dataset", "dataset-nomodified"),
+        ("Dataset", "dataset-nopublisher"),
+        ("Dataset", "dataset-nolicense"),
+        ("Dataset", "dataset-nodescription"),
     ],
 )
 def test_core_shapes_negative(shacl, example):
     shacl_graph = get_shacl_path(shacl)
-    example_graph = get_example_path(example)
+    example_graph = get_testcase_path(example)
 
     conform, _, result_text = validate(
         data_graph=example_graph,
@@ -44,6 +49,11 @@ def test_core_shapes_negative(shacl, example):
 
 def get_example_path(example):
     return rf"Formalisation(shacl)/Core/Example-Data/{example}.ttl"
+
+
+def get_testcase_path(testcase):
+    return rf"tests/testdata/{testcase}.ttl"
+
 
 def get_shacl_path(shacl):
     return rf"Formalisation(shacl)/Core/PiecesShape/{shacl}.ttl"
